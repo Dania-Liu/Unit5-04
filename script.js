2258aec706990fbb609ec56250fe4ed7d3e0adc2
Dania <dania.liu@ocsbstudent.ca>
2021-03-17 15:11:57 +0000

    Fixed CSS and JavaScript

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-document.getElementById('change-image') .addEventListener('click', image)
-document.getElementById('change-text') .addEventListener('click', text)
+document.getElementById('change-image').addEventListener('click', image)
+document.getElementById('change-text').addEventListener('click', text)
 document.getElementById('hide').addEventListener('click', hide)
-document.getElementById('show') .addEventListener('click', show)
+document.getElementById('show').addEventListener('click', show)
 document.getElementById('bg').addEventListener('click', background)
 
 function image () {
-  document.getElementById('picture').src = "./images/gorilla.jpg"
+  document.getElementById('picture').src = './images/gorilla.jpg'
 }
 
 function text () {
-  document.getElementById('text').innerHTML = "Goodbye, World!"
+  document.getElementById('text').innerHTML = 'Goodbye, World!'
 }
 
 function hide () {
-  document.getElementById('text').style.display = "none"
+  document.getElementById('text').style.display = 'none'
 }
 
 function show () {
-  document.getElementById('text').style.display = "block"
+  document.getElementById('text').style.display = 'block'
 }
 
 function background () {
-  document.body.style.backgroundColor = "red"
+  document.body.style.backgroundColor = 'red'
 }
